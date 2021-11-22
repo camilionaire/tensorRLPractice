9390c168a899f41c0fa3598889943e384016a64c
Camilo <camilo3@pdx.edu>
2021-11-22 02:34:02 -0800

    works. needs save and load functions though.

--- atariAttempt/tutorial.py
+++ atariAttempt/tutorial.py
@@ -1,10 +1,19 @@
 
+from ctypes import windll
 import gym
 import random
-import ale_py
+import ale_py # not 100 if need this.
+import numpy as np
 
+# for building and compiling(Adam) the model
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.layers import Dense, Flatten, Convolution2D
+from tensorflow.keras.optimizers import Adam
+
+# for building the agent(I think?..)
+from rl.agents import DQNAgent
+from rl.memory import SequentialMemory
+from rl.policy import LinearAnnealedPolicy, EpsGreedyQPolicy
 
 def example(env, actions):
 	episodes = 5
@@ -26,6 +35,30 @@ def example(env, actions):
 
 	env.close()
 
+def build_model(height, width, channels, actions):
+
+	model = Sequential()
+
+	# num of filters, size, strides 4,4 means diagonal, activation, input shape (3 allows mult images), hwc
+	model.add(Convolution2D(32, (8,8), strides=(4,4), activation='relu', input_shape=(3, height, width, channels)))
+	model.add(Convolution2D(64, (4,4), strides=(2,2), activation='relu'))
+	model.add(Convolution2D(64, (3,3), activation='relu'))
+	model.add(Flatten())
+
+	# dense layer aka fully connected layer
+	model.add(Dense(512, activation='relu'))
+	model.add(Dense(256, activation='relu'))
+	model.add(Dense(actions, activation='linear'))
+
+	return model
+
+def build_agent(model, actions):
+	policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.2, nb_steps=10000)
+	memory = SequentialMemory(limit=1000, window_length=3)
+	dqn = DQNAgent(model=model, memory=memory, policy=policy,
+				enable_dueling_network=True, dueling_type='avg', 
+					nb_actions=actions, nb_steps_warmup=1000)
+	return dqn
 
 def main():
 
@@ -34,6 +67,7 @@ def main():
 	print('gym:', gym.__version__)
 	print('ale_py:', ale_py.__version__)
 
+	# v0 is just the image data(? maybe?...)
 	env = gym.make('SpaceInvaders-v0')
 	height, width, channels = env.observation_space.shape
 	actions = env.action_space.n
@@ -42,8 +76,22 @@ def main():
 	print(f"actions: {actions}")
 
 	print(env.unwrapped.get_action_meanings())
+	# example random actions from function above.
+	# example(env, actions)
 
-	example(env, actions)
+	model = build_model(height, width, channels, actions)
+
+	model.summary()
+
+	# build the agent for the model / actions and compile and then fit.
+	dqn = build_agent(model, actions)
+	dqn.compile(Adam(learning_rate=1e-4))
+	dqn.fit(env, nb_steps=10000, visualize=False, verbose=2)
+
+	scores = dqn.test(env, nb_episodes=10, visualize=True)
+	print(np.mean(scores.history['episode_reward']))
+
+	env.close()
 
 if __name__ == "__main__":
 	main()
